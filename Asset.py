63e8a2b1696030ba5f26c36e0898144af22641dc
saakshiiamv <ambss001@mymail.unisa.edu.au>
2025-10-18 09:44:13 +1030

    initialise a new asset for class asset

--- Asset.py
+++ Asset.py
@@ -1,8 +1,17 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
+Description: The asset class is the base class for all items in the simulation.
+It represents which digital assests can be encrypted and transferred.
+Author: Sakshi Ambekar
 ID: <student_id>
-Username: <username>
+Username: ambss001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self, name, description):
+        self.__name = name
+        self.__description = description
+        self.__is_encrypted =  # assests default start: unencrypted - boolean
+
+
