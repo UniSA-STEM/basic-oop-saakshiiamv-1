0a97ef4fbacca1edddad49387f31cfdf6e08437f
saakshiiamv <ambss001@mymail.unisa.edu.au>
2025-10-18 09:56:50 +1030

    initialise new rig

--- Rig.py
+++ Rig.py
@@ -1,8 +1,17 @@
 """
 File: Rig.py
-Description: <A brief description of this Python module.>
-Author: <full name>
+Description: The rig class represents the computer system of a hacker.
+Handling capabilities of storage, upgrades, damage, and generating assets.
+Author: Sakshi Ambekar
 ID: <student_id>
-Username: <username>
+Username: ambss001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Rig:
+    def __init__(self, name):
+        self.name = name
+        self.damage_count = 0 # current damage level
+        self.broken_state = False # is the rig functioning?
+        self.storage = [] # storage for assets
+        self.upgrade_level = 0
