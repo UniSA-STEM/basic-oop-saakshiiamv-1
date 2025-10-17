c0e96a8a13b29a0ed189f8f98fb0962ec2ff00f3
saakshiiamv <ambss001@mymail.unisa.edu.au>
2025-10-18 09:48:53 +1030

    string conversion method

--- Asset.py
+++ Asset.py
@@ -10,8 +10,12 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 class Asset:
     def __init__(self, name, description):
-        self.__name = name
-        self.__description = description
-        self.__is_encrypted =  # assests default start: unencrypted - boolean
+        self.name = name
+        self.description = description
+        self.is_encrypted =  # assests default start: unencrypted - boolean
 
-
+    def __str__(self):
+        if self.is_encrypted is True:
+            return f"{self.name} : {self.description} [encrypted]"
+        else:
+            return f"{self.name} : {self.description}"
